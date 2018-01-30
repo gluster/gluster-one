2c668f6f083fc3132962ce686dbf52d7c171e6d4
Dustin Black <dblack@redhat.com>
2018-01-30 13:49:12 -0500

    change failed play logging to info level

--- gluster-colonizer.py
+++ gluster-colonizer.py
@@ -306,8 +306,8 @@ def run_ansible_playbook(playbook, continue_on_fail=False):
             logger.error(stderr)
             abortSetup("Ansible playbook error")
         else:
-            logger.warning(stdout)
-            logger.warning(stderr)
+            logger.info(stdout)
+            logger.info(stderr)
             logger.warning("Continuing deployment; please see logs for failure details.")
 
 
