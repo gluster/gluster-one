e185366ba0a96b35983ce2ea045abe2f6caa5c53
Dustin Black <dblack@redhat.com>
2018-03-22 09:45:38 -0400

    quote netbios name and ad domain name in perf test var pass to ansible

--- gluster-colonizer.py
+++ gluster-colonizer.py
@@ -1606,8 +1606,8 @@ try:
     playbook_args += ',nodes_deployed: ' + str(desiredNumOfNodes)
     playbook_args += ',use_nfs: ' + str(use_nfs)
     playbook_args += ',use_smb: ' + str(use_smb)
-    playbook_args += ',ad_netbios_name: ' + str(ad_netbios_name)
-    playbook_args += ',ad_domain_name: ' + str(ad_domain_name)
+    playbook_args += ',ad_netbios_name: \'' + str(ad_netbios_name) + '\''
+    playbook_args += ',ad_domain_name: \'' + str(ad_domain_name) + '\''
     playbook_args += '}"'
     run_ansible_playbook(playbook_args, continue_on_fail=True)
 
