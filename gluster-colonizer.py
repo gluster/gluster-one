2097731a4d5570ad603c4df8f36a8f49d1884e00
Marko Karg <mkarg@redhat.com>
2018-04-10 05:47:23 -0700

    moved from nmcli con show to nmcli d show to avoid problems with non-standard profile names. This way we rely on the actual interfaces instead of the profile names

--- gluster-colonizer.py
+++ gluster-colonizer.py
@@ -1122,7 +1122,7 @@ try:
             while True:
                 try:
                     host_command('/bin/systemctl start NetworkManager')
-                    p1 = Popen(shlex.split('/bin/nmcli con show %s' % nm_mgmt_interface), stdout=PIPE)
+                    p1 = Popen(shlex.split('/bin/nmcli d show %s' % nm_mgmt_interface), stdout=PIPE)
                     p2 = Popen(shlex.split('grep IP4.ADDRESS\\\\[1\\\\]'), stdin=p1.stdout, stdout=PIPE)
                     p3 = Popen(shlex.split('awk "{print $2}"'), stdin=p2.stdout, stdout=PIPE)
                     p1.stdout.close()
