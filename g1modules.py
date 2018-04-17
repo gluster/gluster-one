70506089035573803b0d697c53a224720f633a14
Dustin Black <dblack@redhat.com>
2018-04-17 15:11:13 -0400

    moved functions back to gluster-colonizer.py because

--- g1modules.py
+++ g1modules.py
@@ -5,42 +5,6 @@ import readline
 from subprocess import *
 import shlex
 
-def setupLogging(args, logfile):
-    # Init logging to log to console screen and file
-    # Create logger
-    logger = logging.getLogger()
-    logger.setLevel(logging.DEBUG)
-    # Create console formatter & handler for logs
-    consoleHandler = logging.StreamHandler()
-    consoleHandler.setLevel(int(args.loglevel))
-    consoleFormatter = logging.Formatter('%(message)s')
-    consoleHandler.setFormatter(consoleFormatter)
-    # Create log file formatter & handler for logs
-    logfileHandler = logging.FileHandler(logfile)
-    logfileHandler.setLevel(logging.DEBUG)
-    logfileFormatter = logging.Formatter(
-        '%(asctime)s - %(levelname)s - %(message)s')
-    logfileHandler.setFormatter(logfileFormatter)
-    # Add handlers to logger
-    logger.addHandler(consoleHandler)
-    logger.addHandler(logfileHandler)
-    return logger
-
-def abortSetup(message=''):
-    # This may be called at any time during the setup process to abort
-    print "\r\n"
-    logger.critical(
-        "Something went wrong and the deployment is being aborted.")
-    if message != '':
-        print "\r\n"
-        logger.critical(message)
-    print "\r\nPlease wait while services are shut down..."
-    stopDhcpService()
-    print "\r\n"
-    logger.critical("Abort complete. Please reboot all nodes and try again.")
-    print "\r\n"
-    sys.exit(1)
-
 def user_input(msg, initial=''):
     # Function to capture raw_input w/ key buffer flush
     tcflush(sys.stdin, TCIOFLUSH)
@@ -74,48 +38,3 @@ def stopDhcpService():
     host_command('/bin/firewall-cmd --remove-service=dhcp')
     host_command('/bin/nmcli con reload %s' % nm_mgmt_interface)
     host_command('/bin/nmcli con up %s' % nm_mgmt_interface)
-
-def killDnsmasq():
-    # Function to stop any existing dnsmasq processes
-    logger.debug("Killing any existing dnsmasq processes")
-    p1 = Popen(shlex.split('ps -e'), stdout=PIPE)
-    p2 = Popen(
-        shlex.split('grep dnsmasq'),
-        stdin=p1.stdout,
-        stdout=PIPE,
-        stderr=STDOUT)
-    pOut, _ = p2.communicate()
-    for line in pOut.splitlines():
-        if 'dnsmasq' in line:
-            pid = int(line.split(None, 1)[0])
-            os.kill(pid, signal.SIGKILL)
-    logger.debug("Wiping the dnsmasq.leases file")
-    host_command("echo '' > /var/lib/dnsmasq/dnsmasq.leases", shell=True)
-
-def host_command(command, shell=False):
-    # Function to execute system commands
-    if shell == True:
-        cmd_args = command
-    else:
-        cmd_args = shlex.split(command)
-
-    logger.debug("Initiating Subprocess: " + command)
-
-    try:
-        cmd_proc = Popen(
-            cmd_args,
-            stdout=PIPE,
-            stderr=STDOUT,
-            universal_newlines=True,
-            shell=shell)
-
-        proc_output, _ = cmd_proc.communicate()
-
-        if proc_output.strip() != "":
-            logger.debug("Subprocess output: " + proc_output)
-    except (OSError, CalledProcessError) as exception:
-        logger.error("Subprocess exception occured: " + str(exception))
-        abortSetup("Subprocess failed")
-
-    return proc_output
-
--- gluster-colonizer.py
+++ gluster-colonizer.py
@@ -179,52 +179,49 @@ if nm_storage_interface.startswith("bond-") or nm_storage_interface.startswith("
 else:
     storage_interface = nm_storage_interface
 
+
+# Init logging to log to console screen and file
+# Create logger
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+# Create console formatter & handler for logs
+consoleHandler = logging.StreamHandler()
+consoleHandler.setLevel(int(args.loglevel))
+consoleFormatter = logging.Formatter('%(message)s')
+consoleHandler.setFormatter(consoleFormatter)
+# Create log file formatter & handler for logs
 logfile = 'gluster-colonizer.log'
-logger = setupLogging(args, logfile)
+logfileHandler = logging.FileHandler(logfile)
+logfileHandler.setLevel(logging.DEBUG)
+logfileFormatter = logging.Formatter(
+    '%(asctime)s - %(levelname)s - %(message)s')
+logfileHandler.setFormatter(logfileFormatter)
+# Add handlers to logger
+logger.addHandler(consoleHandler)
+logger.addHandler(logfileHandler)
+
+def abortSetup(message=''):
+    # This may be called at any time during the setup process to abort
+    print "\r\n"
+    logger.critical(
+        "Something went wrong and the deployment is being aborted.")
+    if message != '':
+        print "\r\n"
+        logger.critical(message)
+    print "\r\nPlease wait while services are shut down..."
+    stopDhcpService()
+    print "\r\n"
+    logger.critical("Abort complete. Please reboot all nodes and try again.")
+    print "\r\n"
+    sys.exit(1)
 
 # NOTE: Moved below to g1modules.py
-## Init logging to log to console screen and file
-## Create logger
-#logger = logging.getLogger()
-#logger.setLevel(logging.DEBUG)
-## Create console formatter & handler for logs
-#consoleHandler = logging.StreamHandler()
-#consoleHandler.setLevel(int(args.loglevel))
-#consoleFormatter = logging.Formatter('%(message)s')
-#consoleHandler.setFormatter(consoleFormatter)
-## Create log file formatter & handler for logs
-#logfile = 'gluster-colonizer.log'
-#logfileHandler = logging.FileHandler(logfile)
-#logfileHandler.setLevel(logging.DEBUG)
-#logfileFormatter = logging.Formatter(
-#    '%(asctime)s - %(levelname)s - %(message)s')
-#logfileHandler.setFormatter(logfileFormatter)
-## Add handlers to logger
-#logger.addHandler(consoleHandler)
-#logger.addHandler(logfileHandler)
-
-#def abortSetup(message=''):
-#    # This may be called at any time during the setup process to abort
-#    print "\r\n"
-#    logger.critical(
-#        "Something went wrong and the deployment is being aborted.")
-#    if message != '':
-#        print "\r\n"
-#        logger.critical(message)
-#    print "\r\nPlease wait while services are shut down..."
-#    stopDhcpService()
-#    print "\r\n"
-#    logger.critical("Abort complete. Please reboot all nodes and try again.")
-#    print "\r\n"
-#    sys.exit(1)
-
 #def user_input(msg):
 #    # Function to capture raw_input w/ key buffer flush
 #    tcflush(sys.stdin, TCIOFLUSH)
 #    keyin = raw_input(msg)
 #    return keyin
-
-
+#
 #def yes_no(answer, do_return=False, default='yes'):
 #    # Simple yes/no prompt function
 #    yes = set(['yes', 'y', 'ye'])
@@ -244,7 +241,7 @@ logger = setupLogging(args, logfile)
 #                abortSetup("Deployment cancelled by user.")
 #        else:
 #            print "Please enter either 'yes' or 'no'\r\n"
-
+#
 #def stopDhcpService():
 #    # Function to stop specialized DHCP server
 #    killDnsmasq()
@@ -252,49 +249,49 @@ logger = setupLogging(args, logfile)
 #    host_command('/bin/nmcli con reload %s' % nm_mgmt_interface)
 #    host_command('/bin/nmcli con up %s' % nm_mgmt_interface)
 
-#def killDnsmasq():
-#    # Function to stop any existing dnsmasq processes
-#    logger.debug("Killing any existing dnsmasq processes")
-#    p1 = Popen(shlex.split('ps -e'), stdout=PIPE)
-#    p2 = Popen(
-#        shlex.split('grep dnsmasq'),
-#        stdin=p1.stdout,
-#        stdout=PIPE,
-#        stderr=STDOUT)
-#    pOut, _ = p2.communicate()
-#    for line in pOut.splitlines():
-#        if 'dnsmasq' in line:
-#            pid = int(line.split(None, 1)[0])
-#            os.kill(pid, signal.SIGKILL)
-#    logger.debug("Wiping the dnsmasq.leases file")
-#    host_command("echo '' > /var/lib/dnsmasq/dnsmasq.leases", shell=True)
+def killDnsmasq():
+    # Function to stop any existing dnsmasq processes
+    logger.debug("Killing any existing dnsmasq processes")
+    p1 = Popen(shlex.split('ps -e'), stdout=PIPE)
+    p2 = Popen(
+        shlex.split('grep dnsmasq'),
+        stdin=p1.stdout,
+        stdout=PIPE,
+        stderr=STDOUT)
+    pOut, _ = p2.communicate()
+    for line in pOut.splitlines():
+        if 'dnsmasq' in line:
+            pid = int(line.split(None, 1)[0])
+            os.kill(pid, signal.SIGKILL)
+    logger.debug("Wiping the dnsmasq.leases file")
+    host_command("echo '' > /var/lib/dnsmasq/dnsmasq.leases", shell=True)
 
-#def host_command(command, shell=False):
-#    # Function to execute system commands
-#    if shell == True:
-#        cmd_args = command
-#    else:
-#        cmd_args = shlex.split(command)
-#
-#    logger.debug("Initiating Subprocess: " + command)
-#
-#    try:
-#        cmd_proc = Popen(
-#            cmd_args,
-#            stdout=PIPE,
-#            stderr=STDOUT,
-#            universal_newlines=True,
-#            shell=shell)
-#
-#        proc_output, _ = cmd_proc.communicate()
-#
-#        if proc_output.strip() != "":
-#            logger.debug("Subprocess output: " + proc_output)
-#    except (OSError, CalledProcessError) as exception:
-#        logger.error("Subprocess exception occured: " + str(exception))
-#        abortSetup("Subprocess failed")
-#
-#    return proc_output
+def host_command(command, shell=False):
+    # Function to execute system commands
+    if shell == True:
+        cmd_args = command
+    else:
+        cmd_args = shlex.split(command)
+
+    logger.debug("Initiating Subprocess: " + command)
+
+    try:
+        cmd_proc = Popen(
+            cmd_args,
+            stdout=PIPE,
+            stderr=STDOUT,
+            universal_newlines=True,
+            shell=shell)
+
+        proc_output, _ = cmd_proc.communicate()
+
+        if proc_output.strip() != "":
+            logger.debug("Subprocess output: " + proc_output)
+    except (OSError, CalledProcessError) as exception:
+        logger.error("Subprocess exception occured: " + str(exception))
+        abortSetup("Subprocess failed")
+
+    return proc_output
 
 
 
