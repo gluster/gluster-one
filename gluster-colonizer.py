e8a8e155b758ca08d83aa373a6e043b9edfcbd65
Dustin Black <dblack@redhat.com>
2018-06-01 14:25:03 -0400

    fix mgmt interface detection

--- gluster-colonizer.py
+++ gluster-colonizer.py
@@ -1073,7 +1073,7 @@ try:
             while True:
                 try:
                     host_command('/bin/systemctl start NetworkManager')
-                    p1 = Popen(shlex.split('/bin/nmcli d show %s' % nm_mgmt_interface), stdout=PIPE)
+                    p1 = Popen(shlex.split('/bin/nmcli d show %s' % mgmt_interface), stdout=PIPE)
                     p2 = Popen(shlex.split('grep IP4.ADDRESS\\\\[1\\\\]'), stdin=p1.stdout, stdout=PIPE)
                     p3 = Popen(shlex.split('awk "{print $2}"'), stdin=p2.stdout, stdout=PIPE)
                     p1.stdout.close()
